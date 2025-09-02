75f5ed8d6d7f9edb64f290327ccbf00eb2a446cd
GEOVANNA SILVA DE SOUSA <25132823@CFP127ALUNO.LOCAL>
2025-09-02 17:01:13 -0300

    estilizando no css

---
+++ app.js
@@ -0,0 +1,42 @@
+import produtos from './produtos.json' with { type: 'json' }
+const container = document.getElementById("container")
+
+let grupoDiv // variável para armazenar a div atual
+
+produtos.forEach((produto, index) => {
+  // A cada 5 produtos cria uma nova div
+  if (index % 4 === 0) {
+    grupoDiv = document.createElement("div")
+    grupoDiv.classList.add("caixa")
+    container.appendChild(grupoDiv)
+  }
+
+  const card = document.createElement("div")
+  card.classList.add("card")
+
+
+  const img = document.createElement("img")
+  img.src = `./img/${produto.imagem}`
+  img.alt = produto.nome
+
+  const nome = document.createElement('p')
+  nome.textContent = produto.nome
+  nome.classList.add("nome")
+
+  const preco = document.createElement('p')
+  preco.textContent = produto.preco
+  preco.classList.add("preco")
+
+  const descricao = document.createElement('p')
+  descricao.textContent = produto.descricao
+
+  const estrelas = document.createElement('div')
+  estrelas.classList.add('estrelas')
+  estrelas.textContent = '★'.repeat(produto.classificacao) + '☆'.repeat(5 - produto.classificacao)
+
+
+
+
+  card.append(img, nome, preco, descricao, estrelas)
+  grupoDiv.appendChild(card)
+})
